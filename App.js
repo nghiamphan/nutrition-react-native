72b4feae703d51e7b7a133e6d24a3e8cd08b1c14
Nghia <19204686+nghiamphan@users.noreply.github.com>
2024-08-11 20:47:14 -0300

    use tab navigation for ScanScreen and TakePhotoScreen

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
 
 import ResultModal from './components/ResultModal'
 import ScanScreen from './components/ScanScreen'
+import TakePhotoScreen from './components/TakePhotoScreen'
 
 const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const TabNavigator = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name="Scan"
+                component={ScanScreen}
+                options={{
+                    title: 'Scan  Product Barcode',
+                }}
+            />
+            <Tab.Screen
+                name="Photo"
+                component={TakePhotoScreen}
+                options={{
+                    title: 'Take Label Photo',
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
 
 export default App = () => {
     return (
@@ -13,16 +37,7 @@ export default App = () => {
             <StatusBar style="auto" />
             <NavigationContainer>
                 <Stack.Navigator>
-                    <Stack.Group>
-                        <Stack.Screen
-                            name="ScanScreen"
-                            component={ScanScreen}
-                            options={{
-                                title: 'Scan  Product Barcode',
-                            }}
-                        />
-                    </Stack.Group>
-
+                    <Stack.Screen name="Main" component={TabNavigator} options={{ headerShown: false }} />
                     <Stack.Group screenOptions={{ presentation: 'modal' }}>
                         <Stack.Screen
                             name="ResultModal"
